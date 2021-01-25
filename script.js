18b4217bff5183c7eb1e7f8763cfc28d5b073ec4
Héber Silvério <hebersilverio@gmail.com>
2021-01-25 13:35:24 -0300

    conclui código

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -10,9 +11,7 @@ function handleKeyUp(event) {
     }
 }
 
-function jump() {
-    let position = 0;
-    
+function jump() {  
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -50,10 +49,15 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         } else {
             cactusPosition -=10;
             cactus.style.left = cactusPosition + 'px';
         }
+        
     }, 20);
 
     setTimeout(createCactus, randomTime);
